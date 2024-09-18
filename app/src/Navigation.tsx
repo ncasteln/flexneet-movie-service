ee95233209b1c4945cc18f425d020f63dd7daba8
NicoCastelnuovo <nico.castelnuovo@outlook.com>
2024-09-18 12:28:28 +0200

    feat: added navbar scrolling behavior with simple transition

--- app/src/Navigation.tsx
+++ app/src/Navigation.tsx
@@ -3,15 +3,29 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { calculateYears } from './utils';
+import { useEffect, useState } from 'react';
 
 interface INavigationProps {
   onClick: React.MouseEventHandler<HTMLElement>,
 }
 
 export default function Navigation({ onClick }: INavigationProps) {
+  const [navBg, setNavBg] = useState(false);
+
+  const changeBg = () => {
+    window.scrollY <= 30 ? setNavBg(false) : setNavBg(true)
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeBg);
+    return () => {
+      window.removeEventListener("scroll", changeBg);
+    }
+  }, [])
+
   return (
     <header>
-      <Navbar fixed="top" expand="sm" className='bg-primary' variant="dark">
+      <Navbar fixed="top" expand="sm" className={`${navBg ? "bg-primary" : "dark"} navbar-transition`} variant="dark">
         <Container>
           <Navbar.Brand>
           <img
